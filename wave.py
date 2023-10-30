838d1442f13ce6bb98a0ee3e7d931c75baceeb44
gamejoon <gamejoon0325@naver.com>
2023-10-30 17:07:08 +0900

    Wave 클래스 제작 중

--- wave.py
+++ wave.py
@@ -1,6 +1,12 @@
-class wave:
+from pico2d import load_image
+
+class Wave:
+    image = None
     def __init__(self):
-        pass
+        self.width = 16
+        self.height = 32
+        if Wave.image == None:
+            Wave.image = load_image("wave.png")
     
     def update(self):
         pass
