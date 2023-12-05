c86e835b8322a40b2407650534987b531b745021
gamejoon <gamejoon0325@naver.com>
2023-12-05 17:27:40 +0900

    임시 저장

--- ball.py
+++ ball.py
@@ -3,7 +3,15 @@ import game_framework
 
 class Ball:
     def __init__(self):
-        pass
+        self.x = 108
+        self.y = 162
+        self.width = 40
+        self.height = 40
+        self.frame = 0
+        self.image = load_image('sprite_sheet.png')
+        self.screen_width = 432
+        self.screen_height = 304
+        self.speed = 3
 
     def update(self):
         pass
